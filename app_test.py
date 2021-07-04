5f80906b9936ade918027b66ca76c042abef97a0
pyb <pbescon@laposte.net>
2021-07-04 19:02:39 +0200

    x

--- app_test.py
+++ app_test.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 
 imp = load(open('imp.pkl','rb'))
 df = load(open('imp.pkl','rb'))
-df = load(open('seuil_stat.pkl','rb'))
+df = load(open('toto.pkl','rb'))
 
 # Add title to the page.
 st.title("Importance")
